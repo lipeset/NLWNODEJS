c253239b89eaf7422d99108f112b24ca806ba50e
Felipe <felipea.lucas@outlook.com>
2021-02-24 14:00:49 -0300

    aula_1

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,12 @@ import express from 'express';
 
 const app = express();
 
+app.get("/", (request, response) => {
+    return response.json({ message: "nlw 04" })
+});
+
+app.post("/", (request, response) => {
+    return response.json({ message: "Os dados foram salvos com sucesso." })
+});
+
 app.listen(3000, () => console.log('server is running'));
