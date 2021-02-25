d267980de78fd76fea5a42bde0451551eb49090e
Felipe <felipea.lucas@outlook.com>
2021-02-25 01:55:33 -0300

    migrations_config

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
 import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "nlw 04" })
-});
-
-app.post("/", (request, response) => {
-    return response.json({ message: "Os dados foram salvos com sucesso." })
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => console.log('server is running'));
